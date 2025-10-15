843a525e4da57d622c40a735cd2c9635f4a3b8ce
Otto Dabernig <DAB240100@spengergasse.at>
2025-10-15 12:10:18 +0100

    Infi hausaufgabe

---
+++ 2025-09-25_sqlite_mit_deno/main.ts
@@ -0,0 +1,8 @@
+import { DatabaseSync } from "node:sqlite";
+
+const db = new DatabaseSync("2ahwii.db");
+const stmt = db.prepare("SELECT * FROM students");
+const rows = stmt.all();
+console.log(rows);
+
+
---
+++ 2025-09-25_sqlite_mit_deno/otto.ts
@@ -0,0 +1,7 @@
+import { DatabaseSync } from "node:sqlite";
+class DatabaseSync
+
+const stmt = db.prepare()
+// sqlite.open("2ahwii.db")
+
+// const db = await sqlite.open("2ahwuu.db");
